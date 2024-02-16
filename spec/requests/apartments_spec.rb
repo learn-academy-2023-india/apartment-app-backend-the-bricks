d624c6f7f1100402011606f0d9e70eebe9957e7d
RoderickVongsa <roderick.vongsa@gmail.com>
2024-02-16 10:19:09 -0800

    read-functionality fixed

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Apartments", type: :request do
       city: "New York",
       state: "NY",
       square_footage: 2000, 
-      price: 13000, 
+      price: "13000", 
       bedrooms: 5, 
       bathrooms: 3, 
       pets: "Puppets only",
@@ -26,6 +26,15 @@ RSpec.describe "Apartments", type: :request do
     apartment = JSON.parse(response.body)
     expect(response).to have_http_status(200)
     expect(apartment.first['street']).to eq("456 Bogus Blvd")
+    expect(apartment.first['unit']).to eq("30")
+    expect(apartment.first['city']).to eq("New York")
+    expect(apartment.first['state']).to eq("NY")
+    expect(apartment.first['square_footage']).to eq(2000)
+    expect(apartment.first['price']).to eq("13000")
+    expect(apartment.first['bedrooms']).to eq(5)
+    expect(apartment.first['bathrooms']).to eq(3)
+    expect(apartment.first['pets']).to eq("Puppets only")
+    expect(apartment.first['image']).to eq("https://upload.wikimedia.org/wikipedia/commons/0/00/Sesame_Street_buildings_%28193090534%29.jpg")
   end
 end
 
